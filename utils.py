b2e8b161cd38f7d2f44fe14a65870440ae8dd56e
Pragadesh Raj <114950806+Pragadesh000@users.noreply.github.com>
2023-02-15 20:06:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://http://Mdisklink.link/api'
+    url = f'http://Mdisklink.link/api'
     params = {'api': SHORTNER_API,
               'url': link,
               }
