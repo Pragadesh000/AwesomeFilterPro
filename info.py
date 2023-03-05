5b0196d2249fda9780f2f2a4881b3f94c5ef8366
Pragadesh Raj <114950806+Pragadesh000@users.noreply.github.com>
2023-03-05 07:14:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001863562543'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001878509207'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TamilanBotsZ_Support')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -65,7 +65,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', 'https://github.com/Pragadesh000/AwesomeFilterPro')
 
-AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 600))
+AUTO_DELETE_SECONDS = int(environ.get('AUTO_DELETE_SECONDS', 1000))
 AUTO_DELETE = environ.get('AUTO_DELETE', True)
 if AUTO_DELETE == "True":
     AUTO_DELETE = True
