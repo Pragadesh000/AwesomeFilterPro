28953dc95132613bb736729cd6a5c02378afac3b
Pragadesh Raj <114950806+Pragadesh000@users.noreply.github.com>
2023-05-08 23:13:04 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'http://tnlink.in/api'
+    url = f'http://tnshort.net/api'
     params = {'api': SHORTNER_API,
               'url': link,
               }
